be16dd04bc1e57b3faa3c3e943c42be953ac7c6f
nalam <thecrystal2014@gmail.com>
2021-04-21 22:51:34 +0700

    nop

--- homeword_interrupt/Core/Src/main.c
+++ homeword_interrupt/Core/Src/main.c
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -65,16 +66,17 @@ typedef struct {
 	uint32_t EMR;
 	uint32_t RTSR;
 	uint32_t FTSR;
+	uint32_t SWIER;
+	uint32_t PR;
 
 } EXTI_t;
 
-void function_intterupt() {
+void EXTI15_10_IRQHandler() {
 	GPIO_t *PORTB0 = 0x40010C00;
-	PORTB0->CRL &= ~(0x0011);
-	PORTB0->ODR |= (1);
-	PORTB0->ODR &= ~(1 << 1);
+	PORTB0->CRL |= (0x0011);
+	PORTB0->ODR &= ~(1);
 	HAL_Delay(100);
-	PORTB0->ODR |= (1 << 1);
+	PORTB0->ODR |= (1);
 	HAL_Delay(100);
 
 }
@@ -86,26 +88,7 @@ void function_intterupt() {
  */
 int main(void) {
 	/* USER CODE BEGIN 1 */
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-// set port pa11
-	GPIO_t *PORTA11 = 0x400108000;
-	PORTA11->CRH &= ~(0b111 << 15);
-	PORTA11->CRH |= (0b1000 << 15);
-// set led output b0
-	GPIO_t *PORTB0 = 0x40010C00;
-	PORTB0->CRL &= ~(0x0011);
-	PORTB0->ODR |= (1);
-// set interrupt function
-	uint32_t *EXTI3_IRQHandler = 0x00000064;
-	*EXTI3_IRQHandler = function_intterupt;
-//  set pin interrupt
-	uint32_t *AFIO_EXTICR3 = 0x40010010;
-	*AFIO_EXTICR3 &= ~(0b1111 << 15);
-// set interrupt mode
-	EXTI_t *PORTA11_interrupt = 0x40010400;
-	PORTA11_interrupt->IMR |= (1 << 11);
-	PORTA11_interrupt->FTSR |= (1 << 11);
+
 	/* USER CODE END 1 */
 
 	/* MCU Configuration--------------------------------------------------------*/
@@ -125,8 +108,31 @@ int main(void) {
 	/* USER CODE END SysInit */
 
 	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
 	/* USER CODE BEGIN 2 */
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+// set port pa11
+	GPIO_t *PORTA11 = 0x40010800;
+	PORTA11->CRH &= ~(0xffffffff);
+	PORTA11->CRH |= (0b1000 << 12);
+	PORTA11->ODR |= (0b1 << 11);
 
+// set led output b0
+	GPIO_t *PORTB0 = 0x40010C00;
+	PORTB0->CRL &= ~(0xffffffff);
+	PORTB0->CRL |= (0b00110011);
+	PORTB0->ODR |= (0xffff);
+	PORTB0->ODR |= (0b11 << 0);
+
+//  set pin interrupt
+	uint32_t *AFIO_EXTICR3 = 0x40010010;
+	*AFIO_EXTICR3 &= ~(0b1111 << 12);
+/// set interrupt mode
+	EXTI_t *PORTA11_interrupt = 0x40010400;
+	PORTA11_interrupt->IMR |= (1 << 11);
+	PORTA11_interrupt->FTSR |= (1 << 11);
+	PORTA11_interrupt->RTSR |= (1 << 11);
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
@@ -135,8 +141,16 @@ int main(void) {
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
-	}
-	/* USER CODE END 3 */
+
+		if (((PORTA11->IDR >> 11) & 1) == 0) {
+
+			PORTB0->ODR &= ~(0b1<<1 );
+		} else {
+			PORTB0->ODR |= (0b1<<1 );
+		}
+
+}
+/* USER CODE END 3 */
 }
 
 /**
@@ -144,31 +158,43 @@ int main(void) {
  * @retval None
  */
 void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+/** Initializes the RCC Oscillators according to the specified parameters
+ * in the RCC_OscInitTypeDef structure.
+ */
+RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+	Error_Handler();
+}
+/** Initializes the CPU, AHB and APB buses clocks
+ */
+RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+		| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
-		Error_Handler();
-	}
+if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+	Error_Handler();
+}
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+
+/* GPIO Ports Clock Enable */
+__HAL_RCC_GPIOA_CLK_ENABLE();
+
 }
 
 /* USER CODE BEGIN 4 */
@@ -180,12 +206,12 @@ void SystemClock_Config(void) {
  * @retval None
  */
 void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
-	/* User can add his own implementation to report the HAL error return state */
-	__disable_irq();
-	while (1) {
-	}
-	/* USER CODE END Error_Handler_Debug */
+/* USER CODE BEGIN Error_Handler_Debug */
+/* User can add his own implementation to report the HAL error return state */
+__disable_irq();
+while (1) {
+}
+/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
